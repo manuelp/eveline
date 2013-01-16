2666a2811f7f95d36caaef509337790a768ede69
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2013-01-16 23:30:30 +0100

    Basic dynamic archives list

--- src/eveline/views.clj
+++ src/eveline/views.clj
@@ -24,6 +24,22 @@
                                               :rfc822)))
   [:section] (h/content (h/html-snippet (format-content post))))
 
+(defn- build-archive [posts]
+  (letfn [(month-index [post]
+            (let [d (DateTime. (:published post))]
+              (vector (time/year d) (time/month d))))]
+    (sort (group-by month-index posts))))
+
+(h/defsnippet archive-link "archives.html" [:a] [month]
+  [:a] (h/do->
+        (h/set-attr :href (str (first month) "/" (second month)))
+        (h/content (str (first month) "-" (second month)))))
+
+(h/defsnippet archive-items "archives.html" [:nav#archives] [archive]
+  [:li]  (h/clone-for [month (map first archive)]
+                      (h/content (archive-link month))))
+
 (h/deftemplate layout "layout.html" [title posts]
   [:section#posts] (h/content (for [p posts]
-                                (post p))))
+                                (post p)))
+  [:section#sidebar] (h/content (archive-items (build-archive posts))))
