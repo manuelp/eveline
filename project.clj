0bd655c298f1b63b3425de68c77436453f2bfbe7
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2013-02-03 14:16:44 +0100

    Updated friend dependency to 0.1.3

--- project.clj
+++ project.clj
@@ -13,5 +13,5 @@
                  [org.clojure/java.jdbc "0.2.3"]
                  [postgresql/postgresql "9.1-901-1.jdbc4"]
                  [markdown-clj "0.9.16"]
-                 [com.cemerick/friend "0.1.2"]]
+                 [com.cemerick/friend "0.1.3"]]
   :ring {:handler eveline.web/routes})
